e54e0424d47fb128e75a22c3c7d6df5869af065b
joaomarcostg <joaomarcos_tg@outlook.com>
2019-12-20 17:36:22 -0300

    codigo de resolução completo

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ const calcula_cor = (horario) => {
         cor1 = 5 * horario[0][0] + horario[0][1]
         return [cor1]
     }
-
-
 }
 
 const grafo_vazio = (tamanho) => {
@@ -50,16 +48,22 @@ const carregar_grafo = (disciplinas) => {
 
     let k = 0
 
+    //associo capacidade e fluxo às suas respectivas posições
     grafo[0][0] = capacidade
     grafo[vertices_count - 1][vertices_count - 1] = -capacidade
 
+    //para cada disciplina
     disciplinas.forEach((disc, i) => {
 
         //faço uma relacao dos codigos das disciplinas e seus respectivos vertices na matriz
+        //cada disciplina terá uma linha respectiva
         codigos_disc.push([(i + 1), disc.codigo])
 
+        //para a linha i==0, associo a coluna de disciplinas com sua carga horária,
+        //assim j para i==0 equivale à linha que essa determinada disciplina 'j' se encontra
         grafo[0][i + 1] = disc.horas
 
+        //para cada turma de determinada disciplina
         disc.turmas.forEach((turma, j) => {
 
             //faço uma relacao dos numeros das turmas e seus respectivos vertives na matriz
@@ -68,7 +72,8 @@ const carregar_grafo = (disciplinas) => {
             //acho a cor que devo pintar a aresta
             const cores = calcula_cor(turma.horario)
 
-            // (j+1) somo 1 devido ao vértice 'S'
+            //marco a posicao no grafo, salvando uma 'tupla', com a carga horária e as cores
+            //j+1) somo 1 devido ao vértice 'S'
             grafo[i + 1][(j + 1) + k + disciplinas.length] = [disc.horas, cores]
             grafo[(j + 1) + k + disciplinas.length][vertices_count - 1] = [disc.horas, cores]
         })
@@ -142,67 +147,24 @@ const disciplinas = [
         'horas': 2,
         'turmas': [{
             'numero': 31,
-            'horario': [[2, 2], [4,1]]
+            'horario': [[2, 2], [4, 1]]
         }]
     }
 ]
 
-const resolver = (grafo, min, j, combinacoes, cores_usadas, fluxo,a) => {
-    
-    if(j < (grafo[0].length - 1)){
-
-        
-        const i = 0
-        const cores_usadas_c = JSON.parse(JSON.stringify(cores_usadas))
-        
-        const grafo_c = JSON.parse(JSON.stringify(grafo))
-        
-        if (grafo_c[i][j] != 0) {
-            
-            //marco a aresta de disciplina como visitada
-            grafo_c[i][j] -= grafo_c[i][j]
-            
-            //digo que nessa aresta passou o fluxo, e a capacidade diminui
-            fluxo += grafo_c[i][j]
-            grafo_c[0][0] -= fluxo
-
-            //dado a disciplina escolhida 'j' escolho a turma em questao
-            // console.log(cores_usadas_c)
-            const resp = escolher_turma(grafo_c, j, cores_usadas_c)
-
-            if (resp.length > 0) {
-
-                resp.forEach( e => {
-                    // console.log(e)
-                    const combinacoes_c = JSON.parse(JSON.stringify(combinacoes))
-                    combinacoes_c.push(e)
-                    if(j == 5){
-                        console.log(combinacoes_c)
-                        a.push(combinacoes_c)
-                    }
-                    resolver(grafo_c, min, j+1, combinacoes_c, cores_usadas_c, fluxo,a)
-                })
-            }
-        }
-    }
-    else{
-        return combinacoes
-    }
-}
-
 const escolher_turma = (grafo, linha, cores_usadas) => {
-    
+
     const resp = []
     let j = linha + 1
-    
+
     while (j < grafo[linha].length) {
-        
+
         if (j != linha && grafo[linha][j] != 0) {
-            // console.log(j)
+
             //marco a aresta de disciplina como visitada
             grafo[linha][j][0] -= grafo[linha][j][0]
-            
-            
+
+
             //pego as cores da aresta para ver se ela pode ser escolhida
             cor1 = grafo[linha][j][1][0]
             cor2 = grafo[linha][j][1][1]
@@ -211,22 +173,106 @@ const escolher_turma = (grafo, linha, cores_usadas) => {
 
             //se as cores estiverem disponiveis elas agora sao removidas
             if ((ind1 == -1) && (ind2 == -1)) {
-                cores_usadas.push(ind1)
-                cores_usadas.push(ind2)
+                cores_usadas.push(grafo[linha][j][1][0])
+                cores_usadas.push(grafo[linha][j][1][1])
                 resp.push([linha, j])
             }
         }
         j++
     }
-    resp.push([null])
+    resp.push('')
     return resp
 }
 
+const resolver = (grafo, min, j, size, combinacoes, cores_usadas, fluxo, possibilidades) => {
+
+    //uso o passo recursivo para evitar que ele alcance o 'j' respectivo à linha 'T'
+    if (j < size) {
+
+        const i = 0
+
+        //faço uma copia dos argumentos originais
+        const cores_usadas_c = JSON.parse(JSON.stringify(cores_usadas))
+        const grafo_c = JSON.parse(JSON.stringify(grafo))
+        let fluxo_c = JSON.parse(JSON.stringify(fluxo))
+
+        //se o grafo[i][j] != 0, há uma capacidade de correr um fluxo ali
+        if (grafo_c[i][j] != 0) {
+
+            //digo que nessa aresta passou o fluxo, e a capacidade diminui
+            fluxo_c = fluxo_c + grafo_c[i][j]
+            grafo_c[0][0] -= fluxo_c
+
+            //marco a aresta de disciplina como visitada
+            grafo_c[i][j] -= grafo_c[i][j]
+
+            //dado a disciplina escolhida 'j' escolho as turmas disponiveis
+            const resp = escolher_turma(grafo_c, j, cores_usadas_c)
+
+            //para cada turma disponivel (incluindo null)
+            resp.forEach(e => {
+
+                //faço a copia das combinacoes anteriores e adiciona essa nova combinacao à lista
+                const combinacoes_c = JSON.parse(JSON.stringify(combinacoes))
+                combinacoes_c.push(e)
+
+                //se j atingir o limite de disciplinas, posso interromper a execucao
+                if (j == size - 1) {
+
+                    //conto quantas turmas nulas existem
+                    let count = 0
+                    combinacoes_c.forEach(e => {
+                        if (e == '') {
+                            count++
+                        }
+                    })
+
+                    //so adiciono ao vetor de respostas as combicaoes que atendem o requisito min
+                    if (combinacoes_c.length - count >= min) {
+                        possibilidades.push(combinacoes_c)
+                    }
+
+                    //no fim de tudo interrompo a execucao
+                    return true
+                }
+
+                //caso ainda nao atingir o limite, resolvo novamente para j+1 e as copias respectivas
+                resolver(grafo_c, min, j + 1, size, combinacoes_c, cores_usadas_c, fluxo_c, possibilidades)
+            })
+        }
+        //caso nao, repito o processo, com a prox disciplina
+        else {
+            resolver(grafo_c, min, j + 1, size, combinacoes, cores_usadas, fluxo, possibilidades)
+        }
+    }
+    //se j ultrapassar o tamanho do grafo, 
+    else {
+        return true
+    }
+}
+
+
+//converto a resposta para algo mais legivel
+const converter = (array, grafo) => {
+
+    array.forEach(comb => {
+        for (let i = 0; i < comb.length; i++) {
+            const disc = comb[i]
+            if (disc != '') {
+                disc[0] = grafo[disc[0]][disc[0]]
+                disc[1] = grafo[disc[1]][disc[1]]
+                comb[i] = disc.toString().replace(',', ' - ')
+            }
+        }
+    })
+}
 
 const combinacoes = [], cores_usadas = []
 let fluxo = 0
 const grafo = carregar_grafo(disciplinas)
-const a = []
-const b = resolver(grafo, 4, 1, combinacoes, cores_usadas, fluxo, a)
+const possibilidades = []
 
-console.log(a)
+resolver(grafo, 4, 1, disciplinas.length + 1, combinacoes, cores_usadas, fluxo, possibilidades)
+converter(possibilidades, grafo)
+
+console.log(possibilidades)
