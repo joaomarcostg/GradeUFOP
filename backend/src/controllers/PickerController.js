7e1b751110ff6236edacf78c7277d8b0193b1e2e
João Marcos Torres Gardingo <45579061+joaomarcostg@users.noreply.github.com>
2020-02-03 16:09:18 -0300

    update PickerController.js

--- backend/src/controllers/PickerController.js
+++ backend/src/controllers/PickerController.js
@@ -1,20 +1,10 @@
 const {knex} = require('../models/pg.js')
-// const knex = require('knex')({
-//     client: 'pg',
-//     connection: {
-//         host: '127.0.0.1',
-//         user: 'joaomarcostg',
-//         password: '5h4z4mCARAI',
-//         database: 'GradeApp'
-//     }
-// })
 
 module.exports = {
     
     async deptos (req, res) {
         try{
             const departamentos = await knex.select('*').from('Departamento')
-            // console.log(departamentos)
             return res.json(departamentos)
         }
         catch(err){
@@ -25,9 +15,7 @@ module.exports = {
     async discs (req, res) {
         try{
             const {depto} = req.headers 
-            // const disciplinas = await knex.select('*').from('Disciplina').where({deptoDisc: depto})
             const disciplinas = await  knex.distinct('codigoDisc', 'nomeDisc').from("Disciplina").where({deptoDisc: depto}).orderBy("nomeDisc")
-            // console.log(disciplinas)
             return res.json(disciplinas)
         }
         catch(err){
